4733f1116822643cc68c7fd718a4bdce99255d86
Carlos Silva <silvacj90@gmail.com>
2023-12-05 21:34:00 -0500

    fix client.cjs client url

--- db/client.cjs
+++ db/client.cjs
@@ -1,10 +1,11 @@
 const { Client } = require("pg");
-const client = new Client({
+const connection = process.env.DATABASE_URL || {
   user: "postgres",
   password: "password",
   host: "localhost",
   port: 5432,
   database: "adopt_a_bot",
-});
+};
+const client = new Client(connection);
 
 module.exports = client;
